778c5cd0cdb249b78a2497cde5f1f97f9d1d79c2
lydia <ldy.0504@163.com>
2018-04-07 23:42:21 -0700

    Initial commit

---
+++ src/main/java/com/example/mac/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.mac.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("ldy", "onCreate: wo zai zheli  ");
+    }
+}
